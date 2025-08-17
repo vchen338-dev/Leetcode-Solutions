f2f45818aa26987f6f374c3fa7b98639fb7b1000
Jochen-code <vchen338@gmail.com>
2025-08-17 22:22:22 +0800

    Add files via upload

---
+++ 12. Integer to Roman.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<pair<int, string>> valueSymbols = {
+            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+            {100, "C"},  {90, "XC"},  {50, "L"},  {40, "XL"},
+            {10, "X"},   {9, "IX"},   {5, "V"},   {4, "IV"},
+            {1, "I"}
+        };
+
+        string result;
+        for (auto &vs : valueSymbols) {
+            while (num >= vs.first) {
+                result += vs.second;
+                num -= vs.first;
+            }
+        }
+        return result;
+    }
+};
